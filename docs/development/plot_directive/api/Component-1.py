073bd7eeba4a53ab804d27a2063fc9ba9295dc90
Charlles Abreu <craabreu@gmail.com>
2026-01-03 00:20:58 +0000

    Docs from refs/heads/main f879a25a7d8746bba4801158d9a58c27b6ea35d0

---
+++ docs/development/plot_directive/api/Component-1.py
@@ -0,0 +1,14 @@
+from importlib.resources import files
+from cosmolayer.sac import Component
+from matplotlib import pyplot as plt
+path = files("cosmolayer.data") / "C=C(N)O.cosmo"
+component = Component(path)
+fig, ax = plt.subplots(figsize=(8, 4))
+grid = component.get_sigma_grid()
+for s in ["NHB", "OH", "OT"]:
+    _ = ax.plot(grid, component.get_sigma_profile(s), label=s)
+_ = ax.plot(grid, component.get_sigma_profile(), label="Overall")
+_ = ax.set_xlabel("Charge density (e/Å²)")
+_ = ax.set_ylabel("Surface area contribution (Å²)")
+_ = ax.legend()
+fig.tight_layout()
---
+++ docs/development/plot_directive/api/create_cosmo_sac_2002_matrix-1.py
@@ -0,0 +1,7 @@
+from cosmolayer.sac import create_cosmo_sac_2002_matrix
+from matplotlib import pyplot as plt
+matrix = create_cosmo_sac_2002_matrix(298.15)
+fig, ax = plt.subplots(figsize=(8, 6))
+im = ax.imshow(matrix, cmap="Spectral")
+_ = fig.colorbar(im, ax=ax, label="ΔW/(RT)")
+fig.tight_layout()
