58f37f621f47621a7c078e68c67c7cce0d863246
Charlles Abreu <craabreu@gmail.com>
2026-02-03 04:15:04 +0000

    Docs from refs/heads/main 3a01c2d50ea7d4b4f3cc1087ab18cdb885d28331

--- docs/development/plot_directive/api/Component-2.py
+++ docs/development/plot_directive/api/Component-2.py
@@ -4,8 +4,8 @@ from matplotlib import pyplot as plt
 path = files("cosmolayer.data") / "C=C(N)O.cosmo"
 component = Component(path)
 fig, ax = plt.subplots(figsize=(8, 4))
-log_p = component.get_log_probabilities()
-_ = ax.bar(range(len(log_p)), np.exp(log_p))
+p = component.get_probabilities()
+_ = ax.bar(range(len(p)), p)
 _ = ax.set_xlabel("Segment type index")
 _ = ax.set_ylabel("Probability")
 fig.tight_layout()
