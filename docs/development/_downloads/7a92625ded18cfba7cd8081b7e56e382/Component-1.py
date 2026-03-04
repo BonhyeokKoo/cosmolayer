0b9ecdb3b105325d3e0b3b28bb5c359563572b28
Charlles Abreu <craabreu@gmail.com>
2026-03-04 16:30:04 +0000

    Docs from refs/heads/main cb5f84f4c6493085b0c2785f035addefd5ef464b

--- docs/development/_downloads/7a92625ded18cfba7cd8081b7e56e382/Component-1.py
+++ docs/development/_downloads/7a92625ded18cfba7cd8081b7e56e382/Component-1.py
@@ -1,13 +1,13 @@
 from importlib.resources import files
 from cosmolayer.cosmosac import Component
+from cosmolayer.cosmosac.segment_groups import SEGMENT_GROUPS
 from matplotlib import pyplot as plt
 path = files("cosmolayer.data") / "C=C(N)O.cosmo"
-component = Component(path.read_text())
+component = Component(path.read_text(), merge_profiles=False)
 fig, ax = plt.subplots(figsize=(8, 4))
-grid = component.get_sigma_grid()
-for s in ["NHB", "OH", "OT"]:
-    _ = ax.plot(grid, component.get_sigma_profile(s), label=s)
-_ = ax.plot(grid, component.get_sigma_profile(), label="Overall")
+grid = component.sigma_grid
+for i, label in enumerate(SEGMENT_GROUPS):
+    _ = ax.plot(grid, component.sigma_profile[i], label=label)
 _ = ax.set_xlabel("Charge density (e/Å²)")
 _ = ax.set_ylabel("Surface area contribution (Å²)")
 _ = ax.legend()
--- docs/development/_downloads/8bbd205314ae734e88e3fbc8ccee1dc0/Component-2.py
+++ docs/development/_downloads/8bbd205314ae734e88e3fbc8ccee1dc0/Component-2.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 path = files("cosmolayer.data") / "C=C(N)O.cosmo"
 component = Component(path.read_text())
 fig, ax = plt.subplots(figsize=(8, 4))
-p = component.get_probabilities()
+p = component.probabilities
 _ = ax.bar(range(len(p)), p)
 _ = ax.set_xlabel("Segment type index")
 _ = ax.set_ylabel("Probability")
